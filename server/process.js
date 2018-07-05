aa0e3a9bbbb4dcd8edeeec4f1f9fe0d637cd948f
Kenneth Black <kenneth.black@live.com>
2018-07-04 17:25:54 -0700

    Make process function for scrape data

--- server/process.js
+++ server/process.js
@@ -1,8 +1,20 @@
-let process = require('./utils/processScrapes');
+const _ = require('lodash');
+const process = require('./utils/processScrapes');
+const Scrape = require('./models').scrape;
+
 
 (async () => {
 
-
-  process('cc7e20e96d70f663589237152de3ca9d17a94f44');
-
+  Scrape.findAll({
+    where: {
+      status: 'new'
+    }
+  }).then((data) => {
+    // console.log(data);
+    let hashes = _.uniqBy(data, 'hash').map(d => d.hash);
+    console.log(hashes);
+    hashes.forEach(hash => {
+      process(hash);
+    });
+  });
 })();
